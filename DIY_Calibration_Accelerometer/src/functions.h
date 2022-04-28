bcafc25254d3a1b0d7a6455166b4a539292e3f15
Brett Olson <brettolson@cox.net>
2022-04-28 07:47:34 -0700

    Reverted to all code in main(). Added forward function declarations

--- DIY_Calibration_Accelerometer/src/functions.h
+++
@@ -1,9 +0,0 @@
-
-void printParam(char txt[], float param[3]);
-void printSetParam(char txt[], float param[3]);
-void calibrateAccelMenu(uint8_t accelFSindex, uint8_t accelODRindex);
-void calibrateAccel(uint16_t NofSamples);
-char readChar();
-void readAnswer(char msg[], uint16_t& param);
-void raw_N_Accel(uint16_t N, float& averX, float& averY, float& averZ);
-
